cab45a5f3cb11fe4ac0d2b65b335df3478e41fe1
Than Doan Thuan <91654840+thuankent@users.noreply.github.com>
2022-04-27 21:16:03 +0700

    Update split audio word (Change naming convention) 27/4

--- custom/SplitAudioWord.py
+++ custom/SplitAudioWord.py
@@ -12,6 +12,30 @@ import os
 import wavio as wv
 import textgrid # textgrid is a useful module to read the textgrid file type
 
+id_to_filename = {1: 'mootj_1', 2: 'trawm_1', 3: 'tams_1', 4: 'muowi_1', 5: 'hai_1'
+                  , 6: 'saus_1', 7: 'trawm_2', 8: 'ler_1', 9: 'saus_2', 10: 'mootj_2'
+                  , 11: 'trawm_3', 12: 'ba_1', 13: 'muowi_2', 14: 'tams_2', 15: 'bayr_1'
+                  , 16: 'muowi_3', 17: 'hai_2', 18: 'tams_3', 19: 'trawm_4', 20: 'tams_4'
+                  , 21: 'muowi_4', 22: 'ba_2', 23: 'chins_1', 24: 'trawm_5', 25: 'tams_5'
+                  , 26: 'muowi_5', 27: 'saus_3', 28: 'mootj_3', 29: 'trawm_6', 30: 'ba_3'
+                  , 31: 'muowi_6', 32: 'saus_4', 33: 'bayr_2', 34: 'trawm_7', 35: 'bayr_3'
+                  , 36: 'muowi_7', 37: 'ba_4', 38: 'mootj_4', 39: 'trawm_8', 40: 'chins_2'
+                  , 41: 'lawm_1', 42: 'chins_3', 43: 'trawm_9', 44: 'bayr_4', 45: 'muowi_8'
+                  , 46: 'saus_5', 47: 'nawm_1', 48: 'trawm_10', 49: 'saus_6', 50: 'muowi_9'
+                  , 51: 'ba_5', 52: 'hai_3', 53: 'trawm_11', 54: 'boons_1', 55: 'muowi_10'
+                  , 56: 'bayr_5', 57: 'saus_7', 58: 'trawm_12', 59: 'tams_6', 60: 'muowi_11'
+                  , 61: 'ba_6', 62: 'nawm_2', 63: 'trawm_13', 64: 'muowif_1', 65: 'saus_8'
+                  , 66: 'trawm_14', 67: 'chins_4', 68: 'muowi_12', 69: 'moots_1', 70: 'mootj_5'
+                  , 71: 'trawm_15', 72: 'hai_4', 73: 'muowi_13', 74: 'bayr_6', 75: 'tams_7'
+                  , 76: 'trawm_16', 77: 'tams_8', 78: 'muowi_14', 79: 'hai_5', 80: 'mootj_6'
+                  , 81: 'trawm_17', 82: 'saus_9', 83: 'muowi_15', 84: 'bayr_7', 85: 'ba_7'
+                  , 86: 'trawm_18', 87: 'hai_6', 88: 'muowi_16', 89: 'hai_7', 90: 'boons_2'
+                  , 91: 'trawm_19', 92: 'ba_8', 93: 'muowi_17', 94: 'hai_8', 95: 'ba_9'
+                  , 96: 'muowi_18', 97: 'ba_10', 98: 'hai_9', 99: 'trawm_20', 100: 'saus_10'
+                  , 101: 'muowi_19', 102: 'ba_11', 103: 'hai_10', 104: 'trawm_21', 105: 'nawm_3'
+                  , 106: 'muowi_20', 107: 'bayr_8', 108: 'trawm_22', 109: 'bayr_9', 110: 'muowi_21'
+                  , 111: 'saus_11'}
+
 def gen_timestamp(number):
     try:
         filename =  number + '.TextGrid'
@@ -20,7 +44,7 @@ def gen_timestamp(number):
         filename =  number + '.textgrid'
         tg = textgrid.TextGrid.fromFile(filename)
 
-    SILENCE = "sil"
+    SILENCE = ["sil", "silent", "silence"]
     LAST_CHARACTER_FILENAME = -10
 
     # Encoding the order of the word of the transcript appropriately. We would check
@@ -28,7 +52,6 @@ def gen_timestamp(number):
     # we currently are. Thus, the index to find the last character of the file name
     # is -10 (started from the back). These encoding numbers are based on the helper
     # spreadsheet accompany to.
-    encoding = 1
     if filename[LAST_CHARACTER_FILENAME] == '1':
         encoding = 1
     elif filename[LAST_CHARACTER_FILENAME] == '2':
@@ -51,10 +74,12 @@ def gen_timestamp(number):
         try:
             # Access the current interval
             current_interval = tg[0][i]
+            current_annotation = current_interval.mark
+            current_annotation = current_annotation.lower()
             # Simply approach: Eliminate all of the sil intervals. Assign the rest
             # of the word transcipt into an encoding code with start and end
             # intevral can be extracted from the interval class.
-            if(current_interval.mark != SILENCE):
+            if(current_annotation not in SILENCE):
                 start = current_interval.minTime 
                 end = current_interval.maxTime
                 timeStampDict[encoding] = [start, end]
@@ -82,7 +107,7 @@ def split_audio_word_and_save(save_folder_path, file_path):
         splitAudio = data[minAtFrame:maxAtFrame]
         # Rename the sequential audio files to match with the naming convention of
         # the dataset
-        finalNaming = f"{audioFileName_root}_W{str(word).zfill(2)}.wav"
+        finalNaming = audioFileName_root + "_W" + id_to_flename[word] + ".wav"
         save_path = os.path.join(save_folder_path, finalNaming)
         wavfile.write(save_path, rate, splitAudio)   
         print('Save file at', save_path) 
