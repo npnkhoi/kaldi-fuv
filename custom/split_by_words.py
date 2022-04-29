55825142179e7d9de51b096da2b1e8c93440676e
Than Doan Thuan <91654840+thuankent@users.noreply.github.com>
2022-04-29 16:04:29 +0700

    Try to update naming convention: Add prefix W, and add prefix 0 for 1-9

--- custom/split_by_words.py
+++ custom/split_by_words.py
@@ -59,7 +59,7 @@ for path, subdirs, files in os.walk(src_folder):
 
       spk2utt[speaker]['cnt'].setdefault(encoded_text, 0)
       spk2utt[speaker]['cnt'][encoded_text] += 1
-      new_label = f"{speaker}_{encoded_text}_{spk2utt[speaker]['cnt'][encoded_text]}"
+      new_label = f"{speaker}_W_{encoded_text}_{spk2utt[speaker]['cnt'][encoded_text].zfill(2)}"
       spk2utt[speaker]['utt'].append(new_label)
 
       # save .wav (split)
