b376fd9485c6cbd162688f5da689ab84900dcaa6
npnkhoi <npnkhoi@gmail.com>
2022-05-10 15:34:44 +0700

    update split script

--- custom/split_by_words.py
+++ custom/split_by_words.py
@@ -15,21 +15,30 @@ from scipy.io import wavfile
 
 from custom.split_utils import VN2TELEX, get_words_from_textgrid
 
-src_folder = 'Data_Apr27' # source folder
-dest_folder = 'khoi_data' # destination folder
-train_subjects = ['S_S01', 'S_S04', 'S_S05', 'S_S08', 'S_S13', 'S_S18', 'S_S19', 'S_S03',  'S_S06', 'S_S09', 'S_S17']
+SRC_FOLDER = 'Data-May10' # source folder
+AUDIO_FOLDER = 'numbers_audio'
+TRAIN_FOLDER = os.path.join('data', 'train')
+TEST_FOLDER = os.path.join('data', 'test')
+ALL_SUBJECTS = ['S_S01', 'S_S03', 'S_S05', 'S_S07', 'S_S01', 'S_S17']
+TRAIN_SUBJECTS = ALL_SUBJECTS[:-len(ALL_SUBJECTS) // 5 + 1] # split train:test 80:20
 
 # Create folders
-subprocess.run(['rm', '-r', dest_folder])
-subprocess.run(['mkdir', '-p', os.path.join(dest_folder, 'audio')])
-subprocess.run(['mkdir', '-p', os.path.join(dest_folder, 'train')])
-subprocess.run(['mkdir', '-p', os.path.join(dest_folder, 'test')])
+subprocess.run(['rm', '-r', AUDIO_FOLDER])
+subprocess.run(['mkdir', '-p', AUDIO_FOLDER])
+subprocess.run(['mkdir', '-p', TRAIN_FOLDER])
+subprocess.run(['mkdir', '-p', TEST_FOLDER])
+
+# Reset files
+for folder in [TRAIN_FOLDER, TEST_FOLDER]:
+  for filename in ['utt2spk', 'text', 'wav.scp']:
+    subprocess.run(['rm', os.path.join(folder, filename)])
 
 spk2utt = {}
+count_success = 0
 
 # Browse all textgrid files
-for path, subdirs, files in os.walk(src_folder):
-  for name in files:
+for path, subdirs, files in os.walk(SRC_FOLDER):
+  for name in sorted(files):
     if name[-8:].lower() != 'textgrid':
       continue
     
@@ -59,31 +68,41 @@ for path, subdirs, files in os.walk(src_folder):
 
       spk2utt[speaker]['cnt'].setdefault(encoded_text, 0)
       spk2utt[speaker]['cnt'][encoded_text] += 1
-      new_label = f"{speaker}_{encoded_text}_{spk2utt[speaker]['cnt'][encoded_text]}"
+      new_label = f"{speaker}_{encoded_text}_{str(spk2utt[speaker]['cnt'][encoded_text]).rjust(2, '0')}"
       spk2utt[speaker]['utt'].append(new_label)
 
       # save .wav (split)
       minAtFrame = math.floor(rate * interval[0])
       maxAtFrame = math.ceil(rate * interval[1])
       splitAudio = wav_data[minAtFrame:maxAtFrame]
-      dest_filename = os.path.join(dest_folder, 'audio', f'{new_label}.wav')
+      dest_filename = os.path.join(AUDIO_FOLDER, f'{new_label}.wav')
       wavfile.write(dest_filename, rate, splitAudio)
       # print(interval[2], dest_filename)
 
       # save text
-      split = 'train' if speaker in train_subjects else 'test'
-      with open(os.path.join(dest_folder, split, 'text'), 'a') as f:
+      folder = TRAIN_FOLDER if speaker in TRAIN_SUBJECTS else TEST_FOLDER
+      with open(os.path.join(folder, 'text'), 'a') as f:
         f.write(f'{new_label} {interval[2]}\n')
 
       # save wav.scp
-      with open(os.path.join(dest_folder, split, 'wav.scp'), 'a') as f:
+      with open(os.path.join(folder, 'wav.scp'), 'a') as f:
         f.write(f'{new_label} {dest_filename}\n')
       
+      count_success += 1      
+
+print(f'Successfullly split {count_success} audio files.')
 # write spk2utt
-for speaker, value in spk2utt.items():
-  split = 'train' if speaker in train_subjects else 'test'
-  with open(os.path.join(dest_folder, split, 'spk2utt'), 'a') as f:
-    f.write(f'{speaker} ')
+for speaker, value in sorted(spk2utt.items()):
+  folder = TRAIN_FOLDER if speaker in TRAIN_SUBJECTS else TEST_FOLDER
+  with open(os.path.join(folder, 'utt2spk'), 'a') as f:
     for audio in value['utt']:
-      f.write(f'{audio} ')
-    f.write('\n')
+      f.write(f'{audio} {speaker}\n')
+
+# sort the lines in data files
+for folder in [TRAIN_FOLDER, TEST_FOLDER]:
+  for filename in ['utt2spk', 'text', 'wav.scp']:
+    with open(os.path.join(folder, filename), 'r') as f:
+      lines = f.readlines()
+    lines.sort()
+    with open(os.path.join(folder, filename), 'w+') as f:
+      f.writelines(lines)
