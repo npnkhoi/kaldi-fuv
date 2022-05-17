15de6a66c681fa2eb01f8374dbf3608c39e9f455
panda1835 <phuc.le@ug.fuv.edu.vn>
2022-05-17 10:28:03 +0700

    edit confusion matrix code

--- custom/confusion_matrix.py
+++ custom/confusion_matrix.py
@@ -4,6 +4,7 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 
 transcript_to_telex = pd.read_csv("TranscriptToName.csv")
+plt.figure(figsize = (10,10))
 
 def create_confusion_matrix(type):
     with open(f"/home/cs398/kaldi/egs/kaldi-fuv/outputs/out_{type}.txt", "r") as f:
@@ -16,10 +17,10 @@ def create_confusion_matrix(type):
         except:
             act = result.strip()
             pred = ""
-        act = transcript_to_telex.loc[transcript_to_telex["filename"] == act.split("_")[2], "transcript"].iloc[0]
+        act = transcript_to_telex.loc[transcript_to_telex["filename"] == act.split("_")[3], "transcript"].iloc[0]
         actual.append(act)
         prediction.append(pred)
-
+    print(len(actual))
     cf_matrix = confusion_matrix(actual, prediction)
 
     ax = sns.heatmap(cf_matrix, annot=True, cmap='Blues')
@@ -30,9 +31,14 @@ def create_confusion_matrix(type):
 
     ## Ticket labels - List must be in alphabetical order
     tick_labels = transcript_to_telex.transcript.to_list()
+    print(tick_labels)
+    print(set(actual))
+    print(set(prediction))
+
     tick_labels.sort()
     ax.set_xticklabels(tick_labels, rotation=90)
     ax.set_yticklabels(tick_labels, rotation=90)
+
 
     ## Save the visualization of the Confusion Matrix.
     # plt.figure(figsize=(20,20))
