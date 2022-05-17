c904e5ca8f5f193dffc79dd8673ef718c6f103cf
panda1835 <phuc.le@ug.fuv.edu.vn>
2022-05-17 11:25:28 +0700

    update output files

--- output.sh
+++ output.sh
@@ -1,2 +1,2 @@
-../../src/latbin/lattice-best-path ark:'gunzip -c exp/mono/decode/lat.1.gz |' 'ark,t:| utils/int2sym.pl -f 2- exp/mono/graph/words.txt > outputs/out_mono.txt'
+../../src/latbin/lattice-best-path ark:'gunzip -c exp/mono/decode/lat.1.gz |' 'ark,t:| utils/int2sym.pl -f 2- exp/mono/graph/words.txt > outputs/out_mono.txt' 
 ../../src/latbin/lattice-best-path ark:'gunzip -c exp/tri1/decode/lat.1.gz |' 'ark,t:| utils/int2sym.pl -f 2- exp/tri1/graph/words.txt > outputs/out_tri1.txt'
